9abcb7b262c2c46601f53bc1cfe8098ca1bbf179
jaeho.lee <jaeho.lee@bespinglobal.com>
2024-05-31 08:29:37 +0900

    스크립트 수정

--- script/eks/_master.sh
+++ script/eks/_master.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # 클러스터 이름 지정
-export CLUSTER_NAME="devops-saas-cloudformation-0527-eks-cluster"
+export CLUSTER_NAME="devops-saas-cloudformation-0531-eks-cluster"
 
 echo "Starting the deployment process..."
 
 
-chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/argoCD.sh
+chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/eks/argoCD.sh
 ./argoCD.sh
 
-chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/nginx-ingress.sh
+chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/eks/nginx-ingress.sh
 ./nginx-ingress.sh
 
-chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/grafana.sh
+chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/eks/grafana.sh
 ./grafana.sh
 
-chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/setting.sh
+sleep 15
+
+chmod +x /Users/jaeho.lee/workspace/saasfication/saas-infra/script/eks/setting.sh
 ./setting.sh
 
 echo "All deployments completed successfully."
--- script/eks/argoCD.sh
+++ script/eks/argoCD.sh
@@ -20,12 +20,6 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 echo "Patching ArgoCD service to be accessible externally..."
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-# Retrieve ArgoCD initial admin password
-echo "Retrieving ArgoCD initial admin password..."
-#생략 가능..
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-echo "ArgoCD initial admin password retrieved."
-
 # Check ArgoCD server service
 echo "Checking ArgoCD server service..."
 kubectl get svc argocd-server -n argocd
