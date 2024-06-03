bb0e78ef29e0694729cd3ab184dac8416f5e2d64
jaeho.lee <jaeho.lee@bespinglobal.com>
2024-06-03 16:39:47 +0900

    스크립트 수정.

--- script/eks/before-setting.sh
+++ script/eks/before-setting.sh
@@ -13,18 +13,11 @@ echo "Creating namespaces..."
 kubectl create namespace saas
 kubectl create namespace argocd
 kubectl create namespace monitoring
+kubectl create namespace cert-manager
 
 # 노드에 레이블 추가 -> kube manifest 파일로 변경 (node-label.yaml)
 echo "Creating label nodes..."
+#todo : ip-10-0-4-45.ap-northeast-2.compute.internal -> node 그룹명이아닌 생성된 노드 이름으로 수정해야함.
 kubectl label nodes ${PREFIX_NAME}-ManagementNodeGroup node-type=management
 kubectl label nodes ${PREFIX_NAME}-ApplicationNodeGroup node-type=application
 
-
-
-
-
-
-
-
-kubectl label nodes ip-10-0-3-68.ap-northeast-2.compute.internal node-type=management
-kubectl label nodes ip-10-0-4-45.ap-northeast-2.compute.internal node-type=application
